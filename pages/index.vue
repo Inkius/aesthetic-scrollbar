dc95660f412883ae5ff91d3929e96ada4de1e0c9
Ordeeper <pedrordeeper@gmail.com>
2024-06-27 15:09:26 -0300

    feat: Add initial structure

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts" defer>
+import '~/assets/css/custom-scrollbar.css'
+import { OverlayScrollbarsComponent } from "overlayscrollbars-vue";
+const options = {
+    scrollbars: { autoHide: 'move' }
+}
+</script>
+
+<template>
+    <main id="teste">
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+        <p>teste</p>
+    </main>
+    <OverlayScrollbarsComponent :options="options" element="body" defer />
+</template>
